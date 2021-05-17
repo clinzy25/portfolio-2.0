a0f685f01564b0a5e037abba1c4d72728719467e
clinzy25 <conner.linzy25@gmail.com>
2021-05-17 13:00:12 -0400

    main content and layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import '../styles/index.scss';
 
+const navIcon = document.querySelector('.fa-bars');
+const navbar = document.querySelector('#navbar');
+
+navIcon.addEventListener('click', () => {
+    navbar.classList.toggle('visibility');
+});
