be2b84a239d17dc3dc2f707767c813281a6fcb74
clinzy25 <conner.linzy25@gmail.com>
2021-05-17 16:29:49 -0400

    mobile nav and about

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import '../styles/index.scss';
 
-const navIcon = document.querySelector('.fa-bars');
+const menuBars = document.querySelector('.menu-bars');
 const navbar = document.querySelector('#navbar');
+const navLink = document.querySelectorAll('.nav-link');
 
-navIcon.addEventListener('click', () => {
+menuBars.addEventListener('click', () => {
+    menuBars.classList.toggle('change');
     navbar.classList.toggle('visibility');
+});
+
+navLink.forEach((link) => {
+    link.addEventListener('click', () => {
+        navbar.classList.toggle('visibility');
+        menuBars.classList.toggle('change');
+    });
 });
