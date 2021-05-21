d3d1cdeb17fdfe080403846d50cf6f68b1c47e5a
clinzy25 <conner.linzy25@gmail.com>
2021-05-21 15:03:42 -0400

    dark mode first pass

--- src/accessibility.js
+++ src/accessibility.js
@@ -4,6 +4,24 @@ const fontDecBtn = document.querySelector('.font-dec');
 const grayScaleBtn = document.querySelector('.grayscale');
 const contrastBtn = document.querySelector('.high-contrast');
 const html = document.querySelector('html');
+// For Dark Mode
+const backgroundImage = document.querySelector('.about-image');
+const navbar = document.querySelector('#navbar');
+const menuBars = document.querySelector('.menu-bars');
+const menuBar = document.querySelectorAll('.bar');
+const logo = document.querySelector('#logo');
+const navText = document.querySelectorAll('a');
+const navLink = document.querySelectorAll('.nav-link');
+const accessibility = document.querySelector('.accessibility');
+const skillHeader = document.querySelectorAll('h3');
+const skillText = document.querySelectorAll('.skill-text');
+const skillSection = document.querySelectorAll('.skill-section');
+const panel = document.querySelectorAll('.panel');
+const paradigmText = document.querySelectorAll('.paradigm-text');
+const formInput = document.querySelectorAll('.form-input');
+const formMessage = document.querySelector('.text-area');
+const submitBtn = document.querySelector('.submit-btn');
+const footer = document.querySelector('#footer');
 
 let darkMode = true;
 let fontSize = 1;
@@ -43,6 +61,54 @@ const decreaseFontSize = () => {
     disableBtn();
 };
 
+const toggleDarkMode = () => {
+    html.classList.toggle('light-global');
+    navbar.classList.toggle('light-global');
+    menuBars.classList.toggle('light-global');
+    accessibility.classList.toggle('light-text');
+    accessibility.classList.toggle('light-global');
+    navText.forEach((item) => {
+        item.classList.toggle('light-text');
+    });
+    navLink.forEach((item) => {
+        item.classList.toggle('light-border');
+    });
+    menuBar.forEach((item) => {
+        item.classList.toggle('menu-bars-light');
+    });
+
+    backgroundImage.classList.toggle('light-about-image');
+
+    skillHeader.forEach((item) => {
+        item.classList.toggle('light-text');
+    });
+    skillText.forEach((item) => {
+        item.classList.toggle('light-text');
+    });
+    skillSection.forEach((item) => {
+        item.classList.toggle('light-border');
+    });
+    panel.forEach((item) => {
+        item.classList.toggle('light-global');
+    });
+    paradigmText.forEach((item) => {
+        item.classList.toggle('light-text');
+    });
+    formInput.forEach((item) => {
+        item.classList.toggle('light-global');
+        item.classList.toggle('light-text');
+        item.classList.toggle('form-border');
+    });
+    formMessage.classList.toggle('light-global');
+    formMessage.classList.toggle('light-text');
+    formMessage.classList.toggle('form-border');
+    submitBtn.classList.toggle('light-text');
+    footer.classList.toggle('light-global');
+    darkMode = !darkMode;
+    if (darkMode) logo.src = './images/logo-white.png';
+    if (!darkMode) logo.src = './images/logo-black.png';
+};
+
 const toggleGreyScale = () => {
     if (!grayScale) {
         html.classList.add('grayscale');
@@ -71,3 +137,4 @@ fontIncBtn.addEventListener('click', () => increaseFontSize());
 fontDecBtn.addEventListener('click', () => decreaseFontSize());
 grayScaleBtn.addEventListener('click', () => toggleGreyScale());
 contrastBtn.addEventListener('click', () => toggleContrast());
+darkModeBtn.addEventListener('click', () => toggleDarkMode());
