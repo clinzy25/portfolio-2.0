8ca0d53415f1c91ed1984d59db2d2c3248324dd9
clinzy25 <conner.linzy25@gmail.com>
2021-05-24 14:41:51 -0400

    nav fix test 01

--- src/index.js
+++ src/index.js
@@ -54,7 +54,11 @@ navLink.forEach((link) => {
 let prev = 0;
 
 window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
     let scrollTop = window.scrollY;
+    if (scrollTop === 0) {
+        navbar.classList.remove('hide-scroll-down', scrollTop > prev);
+    }
     if (!recentClick || scrollTop - prev > 100) {
         navbar.classList.toggle('hide-scroll-down', scrollTop > prev);
     }
